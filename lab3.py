198fee6ba9dba2610c022a4286158676883b0f42
ahmed00112 <120638186+ahmed00112@users.noreply.github.com>
2022-12-27 16:15:15 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -10,4 +10,42 @@ c=[]
 c.append(9)
 c.insert(0,1)
 c.extend([25,89,0])
-print(c)
+#print(c)
+
+c.pop(2)
+#print(c)
+c.remove(0)
+#print(c)
+c.clear()
+#print(c)
+del c
+#print(c)
+
+#print(nl)
+nl.reverse()
+#print(nl)
+e=[]
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+a=0
+for i in list1:
+    e.append(list1[a]+list2[a])
+    a=a+1
+
+
+#print(e)
+n=(i+j for i ,j in zip(list1,list2))
+c=list(n)
+#print(c)
+
+sq=[1,2,3,4,5,6,7,8,9,10]
+q=[]
+for i in sq:
+    q.append(i**2)
+#print(q)
+
+l1=[10,20,30]
+l2=[100,200,300]
+l2.sort()
+for i, j in zip(l1,l2):
+    print(i,j)
